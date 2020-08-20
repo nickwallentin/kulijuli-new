07bacda86006e755e505291b9bf321c9dea2b2b2
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-08-20 13:06:53 +0200

    layout update

--- src/components/sections/articles.js
+++ src/components/sections/articles.js
@@ -9,9 +9,31 @@ const SectionArticles = () => {
     <React.Fragment>
       <h2>Senaste inläggen</h2>
       <ArticlesLayout>
-        {posts.map(({ node: post }) => (
-          <ArticleItem>{post.frontmatter.title}</ArticleItem>
-        ))}
+        <div className="latest">
+          <article className="latest">
+            <Img
+              fluid={posts[0].node.frontmatter.mainImage.childImageSharp.fluid}
+            />
+
+            {posts[0].node.frontmatter.title}
+          </article>
+        </div>
+        <div className="posts">
+          {posts.map(({ node: post }, index) => {
+            if (index !== 0) {
+              return (
+                <article key={post.id}>
+                  <Img
+                    fluid={post.frontmatter.mainImage.childImageSharp.fluid}
+                  />
+                  <div className="article-content">
+                    {post.frontmatter.title}
+                  </div>
+                </article>
+              )
+            }
+          })}
+        </div>
       </ArticlesLayout>
     </React.Fragment>
   )
@@ -21,6 +43,22 @@ export default SectionArticles
 
 const ArticlesLayout = styled.div`
   display: grid;
-  grid-template-columns: 2fr 1fr 1fr;
+
+  grid-gap: 20px;
+  @media (min-width: 980px) {
+    grid-template-columns: 1fr 2fr;
+  }
+  .posts {
+    display: grid;
+    grid-gap: 20px;
+    @media (min-width: 980px) {
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr;
+    }
+    article {
+      display: grid;
+      grid-gap: 15px;
+      grid-template-columns: 1fr 1fr;
+    }
+  }
 `
-const ArticleItem = styled.article``
