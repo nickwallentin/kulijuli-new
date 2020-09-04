a05aa4c4bf4da7385f53822ca4f7dfb71f0b0e33
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-09-04 12:18:57 +0200

    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-
+    "gatsby-plugin-loadable-components-ssr",
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
