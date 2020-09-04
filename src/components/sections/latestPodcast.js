9f9aaf36a397f744bfd5bc0837e0ff5c51459ad5
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-09-04 11:48:20 +0200

    added lazy podcast to first page

--- src/components/sections/latestPodcast.js
+++ src/components/sections/latestPodcast.js
@@ -1,17 +1,11 @@
 import React from "react"
+import LatestPodcast from "../latestPodcast"
 
 const SectionLatestPodcast = () => {
   return (
     <React.Fragment>
       <h2>Senaste podcastavsnittet</h2>
-      <iframe
-        src="https://open.spotify.com/embed-podcast/show/6vKIHgeMV4duhcdc1w0jeH"
-        width="100%"
-        height="232"
-        frameborder="0"
-        allowtransparency="true"
-        allow="encrypted-media"
-      ></iframe>
+      <LatestPodcast />
     </React.Fragment>
   )
 }
