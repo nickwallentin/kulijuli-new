fb51ffad3dd2cee1a2ec017ae4fc9c99cff7f25d
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-09-07 13:09:44 +0200

    preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-preact`,
     "gatsby-plugin-loadable-components-ssr",
     {
       resolve: `gatsby-plugin-purgecss`,
