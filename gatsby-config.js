764085735a9cd9f88e33afef919cdf6948dc29b5
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-09-07 13:11:04 +0200

    remove preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-preact`,
     "gatsby-plugin-loadable-components-ssr",
     {
       resolve: `gatsby-plugin-purgecss`,
